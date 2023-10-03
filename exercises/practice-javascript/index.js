3b11b68de45f96fec1213b2465040316bea17522
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2023-10-03 14:34:50 -0600

    .

--- exercises/practice-javascript/index.js
+++ exercises/practice-javascript/index.js
@@ -643,7 +643,166 @@ while (i < workers.length) {
   }
   i++;
 }
-//
+///////////////  WOMEWORK HARRISONG
+const people = [
+  {
+    firstName: "Sam",
+    lastName: "Hughes",
+    DOB: {
+      day: 7,
+      month: 7,
+      year: 1978,
+    },
+    department: "Development",
+    salary: "45000",
+  },
+  {
+    firstName: "Terri",
+    lastName: "Bishop",
+    DOB: {
+      day: 2,
+      month: 4,
+      year: 1989,
+    },
+    department: "Development",
+    salary: "35000",
+  },
+  {
+    firstName: "Jar",
+    lastName: "Burke",
+    DOB: {
+      day: 11,
+      month: 1,
+      year: 1985,
+    },
+    department: "Marketing",
+    salary: "38000",
+  },
+  {
+    firstName: "Julio",
+    lastName: "Miller",
+    DOB: {
+      day: 12,
+      month: 7,
+      year: 1991,
+    },
+    department: "Sales",
+    salary: "40000",
+  },
+  {
+    firstName: "Chester",
+    lastName: "Flores",
+    DOB: {
+      day: 3,
+      month: 15,
+      year: 1988,
+    },
+    department: "Development",
+    salary: "41000",
+  },
+  {
+    firstName: "Madison",
+    lastName: "Marshall",
+    DOB: {
+      day: 9,
+      month: 22,
+      year: 1980,
+    },
+    department: "Sales",
+    salary: "32000",
+  },
+  {
+    firstName: "Ava",
+    lastName: "Pena",
+    DOB: {
+      day: 11,
+      month: 2,
+      year: 1986,
+    },
+    department: "Development",
+    salary: "38000",
+  },
+  {
+    firstName: "Gabriella",
+    lastName: "Steward",
+    DOB: {
+      day: 8,
+      month: 26,
+      year: 1994,
+    },
+    department: "Marketing",
+    salary: "46000",
+  },
+  {
+    firstName: "Charles",
+    lastName: "Campbell",
+    DOB: {
+      day: 9,
+      month: 4,
+      year: 1977,
+    },
+    department: "Sales",
+    salary: "42000",
+  },
+  {
+    firstName: "Tiffany",
+    lastName: "Lambert",
+    DOB: {
+      day: 5,
+      month: 11,
+      year: 1990,
+    },
+    department: "Development",
+    salary: "34000",
+  },
+  {
+    firstName: "Antonio",
+    lastName: "Gonzalez",
+    DOB: {
+      day: 3,
+      month: 24,
+      year: 1985,
+    },
+    department: "Office Management",
+    salary: "49000",
+  },
+  {
+    firstName: "Aaron",
+    lastName: "Garrett",
+    DOB: {
+      day: 9,
+      month: 4,
+      year: 1985,
+    },
+    department: "Development",
+    salary: "39000",
+  },
+];
+console.log("This is my People", people);
+// 1) What is the average income of all the people in the array?
+let theSum = 0;
+for (let i = 0; i < people.length; i++) {
+  if (people[i].salary) {
+    theSum = parseInt(people[i].salary) + theSum;
+  }
+}
+
+console.log("percentage of salaried people", theSum / people.length);
+/////////////
+// 2) Who are the people that are currently older than 30?
+let sumincreased = 0;
+for (let i = 0; i < people.length; i++) {
+  if (people[i].DOB.year > 2023 - 30) {
+    console.log(
+      "These people are above thirty years old.",
+      people[i].firstName
+    );
+  }
+}
+/////////
+
+///////////////  WOMEWORK HARRISONG
+
 console.log("This is person", person);
 console.log("This is student", student);
 console.log("This is car", car);
